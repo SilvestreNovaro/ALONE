ca5abad8531ff8624d358b067ab06c09d08eb212
SilvestreNovaro <silvestrenovarohueyo@gmail.com>
2023-03-31 19:13:38 -0300

    City controller completed

--- demo/src/main/java/com/alone/demo/controller/CityController.java
+++ demo/src/main/java/com/alone/demo/controller/CityController.java
@@ -22,21 +22,46 @@ public class CityController {
     private final CitiesService citiesService;
 
     @GetMapping("list")
-    public List<Cities> list(){
+    public List<Cities> list() {
         return citiesService.list();
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> add(@RequestBody Cities cities){
+    public ResponseEntity<?> add(@RequestBody Cities cities) {
         Optional<Cities> optionalCities = citiesService.findByName(cities.getName());
-        Optional <Cities> lookingSameCountry= citiesService.findByCountry(cities.getCountry());
-        if(optionalCities.isPresent()){
+        Optional<Cities> lookingSameCountry = citiesService.findByCountry(cities.getCountry());
+        if (optionalCities.isPresent()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The city " + cities.getName() + " already exists on our registers");
         }
-        if(lookingSameCountry.isPresent()){
+        if (lookingSameCountry.isPresent()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The country" + cities.getCountry() + " already exists on our registers");
         }
         citiesService.add(cities);
         return ResponseEntity.status(HttpStatus.CREATED).body("City added succesfully!");
+    }
+
+    @PutMapping("/modify/{id}")
+    public ResponseEntity<?> updateCity(@RequestBody Cities cities, @PathVariable Long id) {
+        Optional<Cities> optionalCities = citiesService.findById(id);
+        if (optionalCities.isPresent()) {
+            Optional<Cities> sameNameCity = citiesService.findByName(cities.getName());
+            if (sameNameCity.isPresent())
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The city " + cities.getName() + " already exists on our registers");
+        }
+        Optional<Cities> citiesOptional = citiesService.findByCountry(cities.getCountry());
+        if (citiesOptional.isPresent())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The country " + cities.getCountry() + " already exists on our registers");
+        citiesService.updateCity(cities, id);
+        return ResponseEntity.status(HttpStatus.CREATED).body("City updated succesfully!");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete (@PathVariable Long id){
+        Optional<Cities> optionalCities = citiesService.findById(id);
+        if(optionalCities.isPresent()) {
+            citiesService.delete(id);
+            return ResponseEntity.status(HttpStatus.OK).body("City with id " + id + " deleted");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Theres no city with the id " + id);
     }
 }
